44a6da2bb1ada28ab3ea2c2f2668a8191c704d60
Park Loqi Tamaroon <loqi@cruzio.com>
2014-05-13 15:16:39 -0700

    Cleaned up code.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -9,17 +9,13 @@ var getElementsByClassName = function(className){
   return accumulateElementsByClassName(className);
 };
 
-var isElementOfClass = function(domNode, className) {
-  return _.any( domNode.className.split(/\s+/) , function(cname){return cname===className;} );
-};
-
 var accumulateElementsByClassName = function(className, thisDomNode, accumA) {
   thisDomNode = thisDomNode || document.body;
   accumA = accumA || [];
-  if (isElementOfClass(thisDomNode, className)) accumA.push(thisDomNode);
+  if (( new RegExp('^|\s+'+className+'\s+|$') ).exec(thisDomNode.className))
+        accumA.push(thisDomNode);
   var childA = thisDomNode.children;
-  for (var i = 0 ; i < childA.length ; i++) {
-    accumulateElementsByClassName(className, childA[i], accumA);
-  }
+  for (var i = 0 ; i < childA.length ; i++)
+        accumulateElementsByClassName(className, childA[i], accumA);
   return accumA;
 };
