9a48cbae59408a78ef5d1306a5ce60b1305c4081
Park Loqi Tamaroon <loqi@cruzio.com>
2014-05-13 15:02:46 -0700

    Wrote getElementsByClassName()  Passes spec.

--- spec/getElementsByClassNameSpec.js
+++ spec/getElementsByClassNameSpec.js
@@ -20,6 +20,8 @@ describe('getElementsByClassName', function(){
       var expectedNodeList = document.getElementsByClassName('targetClassName');
       var expectedArray = Array.prototype.slice.apply(expectedNodeList);
       var equality = _.isEqual(result, expectedArray); // why can't we use `===` here?
+
+if (!equality) debugger;
       expect(equality).to.equal(true);
 
       $rootElement.remove();
--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -2,8 +2,24 @@
 // var getElementsByClassName = function (className) {
 //   return document.getElementsByClassName(className);
 // };
-
 // But instead we're going to implement it from scratch:
+
+
 var getElementsByClassName = function(className){
-  // your code here
+  return accumulateElementsByClassName(className);
+};
+
+var isElementOfClass = function(domNode, className) {
+  return _.any( domNode.className.split(/\s+/) , function(cname){return cname===className;} );
+};
+
+var accumulateElementsByClassName = function(className, thisDomNode, accumA) {
+  thisDomNode = thisDomNode || document.body;
+  accumA = accumA || [];
+  if (isElementOfClass(thisDomNode, className)) accumA.push(thisDomNode);
+  var childA = thisDomNode.children;
+  for (var i = 0 ; i < childA.length ; i++) {
+    accumulateElementsByClassName(className, childA[i], accumA);
+  }
+  return accumA;
 };
