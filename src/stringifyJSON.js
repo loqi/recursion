cc2b8712814c834e7f38f11fc58ec43e48bd939a
Park Loqi Tamaroon <loqi@cruzio.com>
2014-05-16 13:39:48 -0700

    Minor edit.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,7 +1,3 @@
-// this is what you would do if you liked things to be easy:
-// var stringifyJSON = JSON.stringify;
-// but you don't so you're going to write it from scratch:
-
 var stringifyJSON = function(obj) {
   if (typeof obj == 'undefined') return '';
   if (typeof obj == 'function') return '';
