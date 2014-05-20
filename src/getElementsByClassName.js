386653fb87645ba9d1b6aaf6c2c06ad8440abbf1
Park Loqi Tamaroon <loqi@cruzio.com>
2014-05-19 22:13:40 -0700

    Made getElementsByClassName() regex enabled.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,7 +1,8 @@
 var getElementsByClassName = function(className, thisDomNode, accumA) {
   thisDomNode = thisDomNode || document.body;
   accumA = accumA || [];
-  classRex = (className && className.length > 0) ? new RegExp('(^|\\s+)'+className+'(\\s+|$)') : /.?/;
+  classRex = className instanceof RegExp ? className :
+      ( (className === '') ? /.?/ : new RegExp('(^|\\s+)'+className+'(\\s+|$)') );
   if (classRex.test(thisDomNode.className))  accumA.push(thisDomNode);
   var childA = thisDomNode.children;
   for (var i = 0 ; i < childA.length ; i++)  getElementsByClassName(className, childA[i], accumA);
